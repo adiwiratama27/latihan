e3aa8047b70c9ebe1350fa0ac2f7159f76bbf8da
unknown <adiwiratama27>
2022-07-15 12:06:50 +0800

    first

---
+++ components/Travels.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="grid grid-cols-4 gap-3">
+    <div
+      v-for="travel in travels"
+      :key="travel.id"
+      class="max-w-sm rounded overflow-hidden shadow-lg"
+    >
+      <img class="w-full" :src="travel.photo" alt="Sunset in the mountains" />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">{{ travel.title }}</div>
+        <p class="text-gray-700 text-base">
+          {{ travel.description }}
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#photography</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#travel</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#winter</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      travels: null,
+    };
+  },
+  async created() {
+    const res = await this.$axios.get(
+      "https://62a7032797b6156bff84ef49.mockapi.io/api/travels"
+    );
+    this.travels = res.data;
+  },
+};
+</script>
+
+<style></style>
---
+++ pages/travels.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Travels />
+  </div>
+</template>
+
+<script>
+import Travels from "../components/Travels.vue";
+
+export default {
+  components: {
+    Travels,
+  },
+};
+</script>
+
+<style></style>
